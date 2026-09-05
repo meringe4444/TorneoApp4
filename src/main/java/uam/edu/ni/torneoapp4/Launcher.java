a2fd79546f68990f9861ae297de06bc62f489aad
meringe4444 <ghostform13.st@gmail.com>
2026-09-05 10:44:31 -0600

    Eliminacion de 'launcher'

--- src/main/java/uam/edu/ni/torneoapp4/Launcher.java
+++
@@ -1,9 +0,0 @@
-package uam.edu.ni.torneoapp4;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(TorneoApp.class, args);
-    }
-}
