f6b08467e93fd154ea1d7ca43e19644b3e99c23e
meringe4444 <ghostform13.st@gmail.com>
2026-09-05 10:32:06 -0600

    feat: crear clase Participante con sus atributos

--- src/main/java/Participante.java
+++ src/main/java/Participante.java
@@ -1,3 +1,13 @@
 public class Participante {
 
+    private String nombre;
+    private int edad;
+    private String telefono;
+    private String categoria;
+    private String genero;
+    private String modalidad;
+    private String disciplina;
+    private String caracteristicas;
+    private String estado;
+
 }
